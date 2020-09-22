87efca870b403993ce917602469906e10e2e019d
Alexander <lebedevskiy_s@rambler.ru>
2020-09-22 12:13:15 +0300

    Integrated pvs-studio in my files.

--- Lib/Matrix.cpp
+++ Lib/Matrix.cpp
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 /*double** Set1 (const int n)
 	{
         double **M = NULL;
